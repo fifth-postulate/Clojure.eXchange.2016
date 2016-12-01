246761b524d3c1d48d739255d746b00df2677467
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-12-01 10:39:06 +0100

    Solve brainbow side by side

--- resources/js/application.js
+++ resources/js/application.js
@@ -6,4 +6,6 @@
     Elm.Brainbow.embed(plain);
     var word = $('#word-brainbow')[0];
     Elm.Brainbow.embed(word);
+    var solve = $('#solve-brainbow')[0];
+    Elm.Brainbow.embed(solve);
 })(jQuery, Elm);
